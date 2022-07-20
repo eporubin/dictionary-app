07f5cf54b10c7c03ca7c3d31c19c7c3f37f18673
Elena <33602691+eporubin@users.noreply.github.com>
2022-07-20 15:49:13 +0200

    Created the searchForm component

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import './SearchForm.css';
 
 export default function SearchForm(){
+    let [searchTerm, setSearchTerm]=useState("");
+    function UpdateSearch(event){
+        event.preventDefault()
+        setSearchTerm(event.target.value)
+    }
+    function Search(event){
+        event.preventDefault()
+        return(
+            alert(`searching for ${searchTerm}`)
+        );
+    }
     return (
         <div className="SearchForm">
             <div className="wrapper">
-                <div className="label">Let's embrace unknown</div>
-                <div className="searchBar">
-                    <input className="searchQueryInput" type="text"  placeholder="Search" defaultValue={""} />
+                <div className="label">Let's embrace the unknown</div>
+                <form className="searchBar" onSubmit={Search}>
+                    <input className="searchQueryInput" type="search"  placeholder="Search" defaultValue={""} autoFocus={true} onChange={UpdateSearch} />
                     <button className="searchQuerySubmit" type="submit" >
                          🔍
                     </button>
-                </div>
+                </form>
             </div>
         </div>
     )
